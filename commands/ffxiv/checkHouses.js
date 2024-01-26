6b5ae1f6bd39ff9d22dce8e3434253e539713d51
Sverker <maxime.attala@gmail.com>
2024-01-26 14:17:45 +0100

    extract getHouses function

--- commands/ffxiv/checkHouses.js
+++ commands/ffxiv/checkHouses.js
@@ -22,36 +22,51 @@ module.exports = {
     async execute(interaction) {
         const world = interaction.options.getString("world");
         await interaction.reply(`Checking for houses on ${world}...`);
-        fetch(`https://paissadb.zhu.codes/worlds/${world}`)
-            .then(async response => {
-                const data = await response.json();
-                console.log(data);
+        try {
+            const data = await getHouses(world);
+            let housesPerDisctrict = {
+                "Mist": 0,
+                "The Lavender Beds": 0,
+                "The Goblet": 0,
+                "Shirogane": 0,
+                "Empyreum": 0,
+            }
 
-                let housesPerDisctrict = {
-                    "Mist": 0,
-                    "The Lavender Beds": 0,
-                    "The Goblet": 0,
-                    "Shirogane": 0,
-                    "Empyreum": 0,
-                }
+            const districts = data.districts;
+            for (const district of districts) {
+                const openPlots = district.open_plots;
+                housesPerDisctrict[district.name] = openPlots.length;
+            }
 
-                const districts = data.districts;
-                for (const district of districts) {
-                    const openPlots = district.open_plots;
-                    housesPerDisctrict[district.name] = openPlots.length;
-                }
-
-                await interaction.editReply(`There are ${data.num_open_plots} houses available on ${data.name}!\n
+            await interaction.editReply(`There are ${data.num_open_plots} houses available on ${data.name}!\n
                 Mist: ${housesPerDisctrict["Mist"]}\n
                 The Lavender Beds: ${housesPerDisctrict["The Lavender Beds"]}\n
                 The Goblet: ${housesPerDisctrict["The Goblet"]}\n
                 Shirogane: ${housesPerDisctrict["Shirogane"]}\n
                 Empyreum: ${housesPerDisctrict["Empyreum"]}\n
                 `);
-
-            }).catch(async error => {
+        }catch (error) {
             console.error(error);
-            await interaction.editReply(`There was an error while executing this command!`);
-        });
+            await interaction.editReply(error);
+        }
     },
+}
+
+
+async function getHouses(worldId){
+    try {
+        const response = await fetch(`https://paissadb.zhu.codes/worlds/${worldId}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+    catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
